5c6f802a61b71c85642a54508f8834f7aa59bf97
inbar maimon <maimonin@post.bgu.ac.il>
2021-03-28 17:51:16 +0300

    added service

--- TradingSystem/TradingSystem/Service/UserService.cs
+++ TradingSystem/TradingSystem/Service/UserService.cs
@@ -8,5 +8,20 @@ namespace TradingSystem.Service
     class UserService
     {
         private UserManagement userManagement;
+        private static readonly Lazy<UserService>
+        lazy =
+        new Lazy<UserService>
+            (() => new UserService());
+
+        private UserService()
+        {
+            this.userManagement = UserManagement.Instance;
+        }
+
+        public static UserService Instance { get { return lazy.Value; } }
+        public String signup(string username, string password, string address)
+        {
+            return userManagement.SignUp(username, password, address);
+        }
     }
 }
