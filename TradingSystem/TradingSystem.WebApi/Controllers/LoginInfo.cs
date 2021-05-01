d5864f66c89d1a423d46785b4b226e6c67fe6fc0
romibarel <romib19999@gmail.com>
2021-05-01 15:19:46 +0300

    starting http server

---
+++ TradingSystem/TradingSystem.WebApi/Controllers/LoginInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TradingSystem.WebApi.Controllers
+{
+    public class LoginInfo
+    {
+        public String username;
+        public String password;
+        public String guestusername;
+    }
+}
---
+++ TradingSystem/TradingSystem/Communication/WSS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace TradingSystem.Communication
+{
+    public class WSS
+    {
+        private WebSocketServer wss;
+
+        public WSS()
+        {
+            wss = new WebSocketServer("ws://localhost");
+        }
+
+        public void start()
+        {
+            wss.Start();
+        }
+    }
+}
