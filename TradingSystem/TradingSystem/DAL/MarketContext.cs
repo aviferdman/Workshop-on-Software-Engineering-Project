2beccd73135c7818426947a15d151e2de487235b
maimonin <48507194+maimonin@users.noreply.github.com>
2021-05-14 19:44:42 +0300

    getting started

---
+++ TradingSystem/TradingSystem/DAL/MarketContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using TradingSystem.Business.UserManagement;
+namespace TradingSystem.DAL
+{
+    class MarketContext : DbContext
+    {
+        DbSet<DataUser>
+    }
+}
