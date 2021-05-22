5c7e27c136781f5c88ea9d43e1d0be90734ed800
boaz23 <boaz23mw@gmail.com>
2021-05-22 16:13:32 +0300

    Web API - Shopping cart
    * Added actions to return the current shopping cart and to add a product to the shopping cart

---
+++ TradingSystem/TradingSystem.WebApi/DTO/ShoppingCart/ShoppingBasketProductDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using TradingSystem.Business.Market;
+
+namespace TradingSystem.WebApi.DTO
+{
+    public class ShoppingBasketProductDTO
+    {
+        public string? Name { get; set; }
+        public Guid Id { get; set; }
+        public int Quantity { get; set; }
+        public double Weight { get; set; }
+        public double Price { get; set; }
+        public string? Category { get; set; }
+        public int Rating { get; set; }
+
+        public static ShoppingBasketProductDTO FromProductData(ProductData productData, int quantity)
+        {
+            return new ShoppingBasketProductDTO
+            {
+                Id = productData.pid,
+                Name = productData._name,
+                Category = productData.category,
+                Quantity = quantity,
+                Price = productData._price,
+                Weight = productData._weight,
+                Rating = productData.rating,
+            };
+        }
+    }
+}
---
+++ TradingSystem/TradingSystem.WebApi/DTO/ShoppingCart/ShoppingCartDTO.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using TradingSystem.Business.Market;
+
+namespace TradingSystem.WebApi.DTO
+{
+    public class ShoppingCartDTO : IEnumerable<ShoppingBasketDTO>
+    {
+        public ShoppingCartDTO(IEnumerable<ShoppingBasketDTO> shoppingBaskets)
+        {
+            ShoppingBaskets = shoppingBaskets;
+        }
+
+        public IEnumerable<ShoppingBasketDTO> ShoppingBaskets { get; }
+
+        public static ShoppingCartDTO FromDictionary(Dictionary<Guid, Dictionary<ProductData, int>> dictionary)
+        {
+            return new ShoppingCartDTO
+            (
+                dictionary.Select(shoppingBasket =>
+                {
+                    return new ShoppingBasketDTO
+                    {
+                        Id = shoppingBasket.Key,
+                        Name = null, // TODO: fill
+                        Products = shoppingBasket.Value.Select(product =>
+                        {
+                            return ShoppingBasketProductDTO.FromProductData(product.Key, product.Value);
+                        })
+                    };
+                })
+            );
+        }
+
+        public IEnumerator<ShoppingBasketDTO> GetEnumerator()
+        {
+            return ShoppingBaskets.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)ShoppingBaskets).GetEnumerator();
+        }
+    }
+}
