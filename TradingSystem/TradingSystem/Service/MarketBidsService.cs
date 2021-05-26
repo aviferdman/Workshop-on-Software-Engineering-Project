2bfaff3dfb10c7ec6a3c33cad7fb866b4163ea61
aviferdman <ferdmaa@post.bgu.ac.il>
2021-05-26 10:49:41 +0300

    concurrency bids

--- TradingSystem/TradingSystem/Service/MarketBidsService.cs
+++ TradingSystem/TradingSystem/Service/MarketBidsService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using TradingSystem.Business.Market;
 using TradingSystem.Business.Market.StorePackage;
@@ -12,10 +13,11 @@ namespace TradingSystem.Service
     class MarketBidsService
     {
         private static readonly Lazy<MarketBidsService> instanceLazy = new Lazy<MarketBidsService>(() => new MarketBidsService(), true);
+        static SemaphoreSlim semaphoreSlim;
 
         private MarketBidsService()
         {
-
+            semaphoreSlim = new SemaphoreSlim(1, 1);
         }
 
         public static MarketBidsService Instance => instanceLazy.Value;
@@ -23,15 +25,40 @@ namespace TradingSystem.Service
         // Customers 
         public async Task<Result<Guid>> CustomerCreateBid(String username, Guid storeId, Guid productId, double newBidPrice)
         {
-            return await MarketBids.Instance.CustomerCreateBid(username, storeId, productId, newBidPrice);
+            await semaphoreSlim.WaitAsync();
+            try
+            {
+                return await MarketBids.Instance.CustomerCreateBid(username, storeId, productId, newBidPrice);
+            }
+            finally
+            {
+                semaphoreSlim.Release();
+            }
+
         }
         public async Task<Result<bool>> CustomerNegotiateBid(Guid storeId, Guid bidId, double newBidPrice)
         {
-            return await MarketBids.Instance.CustomerNegotiateBid(storeId, bidId, newBidPrice);
+            await semaphoreSlim.WaitAsync();
+            try
+            {
+                return await MarketBids.Instance.CustomerNegotiateBid(storeId, bidId, newBidPrice);
+            }
+            finally
+            {
+                semaphoreSlim.Release();
+            }
         }
         public async Task<Result<bool>> CustomerDenyBid(Guid storeId, Guid bidId)
         {
-            return await MarketBids.Instance.CustomerDenyBid(storeId, bidId);
+            await semaphoreSlim.WaitAsync();
+            try
+            {
+                return await MarketBids.Instance.CustomerDenyBid(storeId, bidId);
+            }
+            finally
+            {
+                semaphoreSlim.Release();
+            }
         }
         public Result<ICollection<Bid>> GetCustomerBids(string username)
         {
@@ -41,15 +68,39 @@ namespace TradingSystem.Service
         // Owners
         public async Task<Result<bool>> OwnerAcceptBid(string ownerUsername, Guid storeId, Guid bidId)
         {
-            return await MarketBids.Instance.OwnerAcceptBid(ownerUsername, storeId, bidId);
+            await semaphoreSlim.WaitAsync();
+            try
+            {
+                return await MarketBids.Instance.OwnerAcceptBid(ownerUsername, storeId, bidId);
+            }
+            finally
+            {
+                semaphoreSlim.Release();
+            }
         }
         public async Task<Result<bool>> OwnerNegotiateBid(string ownerUsername, Guid storeId, Guid bidId, double newBidPrice)
         {
-            return await MarketBids.Instance.OwnerNegotiateBid(ownerUsername, storeId, bidId, newBidPrice);
+            await semaphoreSlim.WaitAsync();
+            try
+            {
+                return await MarketBids.Instance.OwnerNegotiateBid(ownerUsername, storeId, bidId, newBidPrice);
+            }
+            finally
+            {
+                semaphoreSlim.Release();
+            }
         }
         public async Task<Result<bool>> OwnerDenyBid(string ownerUsername, Guid storeId, Guid bidId)
         {
-            return await MarketBids.Instance.OwnerDenyBid(ownerUsername, storeId, bidId);
+            await semaphoreSlim.WaitAsync();
+            try
+            {
+                return await MarketBids.Instance.OwnerDenyBid(ownerUsername, storeId, bidId);
+            }
+            finally
+            {
+                semaphoreSlim.Release();
+            }
         }
         public Result<ICollection<Bid>> GetOwnerBids(string ownerUsername)
         {
