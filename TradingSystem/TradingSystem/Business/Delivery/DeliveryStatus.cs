111cb09ae1505b4a9f82d61853f515961878ac55
aviferdman <ferdmaa@post.bgu.ac.il>
2021-05-28 13:48:17 +0300

    add store name to delivery status

--- TradingSystem/TradingSystem/Business/Delivery/DeliveryStatus.cs
+++ TradingSystem/TradingSystem/Business/Delivery/DeliveryStatus.cs
@@ -9,13 +9,15 @@ namespace TradingSystem.Business.Delivery
         string _packageId;
         string _username;
         Guid _storeId;
+        string storeName;
         bool _status;
 
-        public DeliveryStatus(string packageId, string username, Guid storeId, bool status)
+        public DeliveryStatus(string packageId, string username, Guid storeId, string storeName, bool status)
         {
             this._packageId = packageId;
             this.Username = username;
             this.StoreId = storeId;
+            this.StoreName = storeName;
             this._status = status;
         }
 
@@ -23,5 +25,6 @@ namespace TradingSystem.Business.Delivery
         public bool Status { get => _status; set => _status = value; }
         public Guid StoreId { get => _storeId; set => _storeId = value; }
         public string Username { get => _username; set => _username = value; }
+        public string StoreName { get => storeName; set => storeName = value; }
     }
 }
