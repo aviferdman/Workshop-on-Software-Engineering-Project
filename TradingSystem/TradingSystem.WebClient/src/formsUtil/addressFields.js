d4b93201a3a292585e9e5dac7fe8ff042301a954
boaz23 <boaz23mw@gmail.com>
2021-05-28 14:24:38 +0300

    Forms fill with dummy data

--- TradingSystem/TradingSystem.WebClient/src/formsUtil/addressFields.js
+++ TradingSystem/TradingSystem.WebClient/src/formsUtil/addressFields.js
@@ -5,14 +5,14 @@ import FormFieldCustomValidation from "./formFieldCustomValidation";
 export default class AddressFields extends FormFields {
     constructor(fields) {
         super(fields || {
-            state: new FormFieldInfo(''),
-            city: new FormFieldInfo(''),
-            street: new FormFieldInfo(''),
-            apartmentNumber: new FormFieldInfo(''),
+            state: new FormFieldInfo('Svenden'),
+            city: new FormFieldInfo('Malmo'),
+            street: new FormFieldInfo('NaM'),
+            apartmentNumber: new FormFieldInfo('12'),
             zipCode: new FormFieldInfo(''),
         });
         // this.fields.apartmentNumber = new FormFieldCustomValidation('', this.validate_digits.bind(this, 'apartmentNumber'));
-        this.fields.zipCode = new FormFieldCustomValidation('', this.validate_digits.bind(this, 'zipCode'));
+        this.fields.zipCode = new FormFieldCustomValidation('123456', this.validate_digits.bind(this, 'zipCode'));
     }
 
     validate_digits(key) {
