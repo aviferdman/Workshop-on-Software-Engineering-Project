c345899b5da9a1c9a62cadf34b67d0c168b36e87
boaz23 <boaz23mw@gmail.com>
2021-06-11 18:04:59 +0300

    Client - store status display
    * Implemeted logic of displaying the status of the bid and the actions selection box

--- TradingSystem/TradingSystem.WebClient/src/pages/Stores/StoreBids/BidRecordStore.js
+++ TradingSystem/TradingSystem.WebClient/src/pages/Stores/StoreBids/BidRecordStore.js
@@ -1,37 +1,48 @@
 import React from "react";
 import {GlobalContext} from "../../../globalContext";
+import * as api from '../../../api';
+import {alertRequestError_default} from "../../../utils";
 
 export default class BidRecordStore extends React.Component {
+    applyAction() {
+    }
+
     render() {
         let bid = this.props.bid;
         let product = bid.product;
+        let isInNegotiation = bid.status === api.data.bids.customerNegotiate || api.data.bids.ownerNegotiate;
 
         return (
-            <div className = "simple-bids-li-div">
-                <p className= "bidName">{<text style={{fontWeight: "bold"}}>User: </text>} {bid.username}  </p>
-                <p className= "bidName"> {<text style={{fontWeight: "bold"}}>Product: </text>} {product.name}  </p>
+            <div className="simple-bids-li-div">
+                <p className="bidName">{<text style={{ fontWeight: "bold" }}>User: </text>} {bid.username}  </p>
+                <p className="bidName"> {<text style={{ fontWeight: "bold" }}>Product: </text>} {product.name}  </p>
                 <p className= "bidName"> {<text style={{fontWeight: "bold"}}>Current Offer:</text>} {bid.price}  </p>
                 <p className= "bidName">{<text style={{fontWeight: "bold"}}>Status:</text>} {
-                    bid.status === "Approved" ? (<label style={{color: "green"}}>Approved</label>):
-                    bid.status === "Negotiation" ? (<label style={{color: "orange"}}>Negotiation</label>):
-                        (<label style={{color: "red"}}>Declined</label>)}
-                </p>
-                <p className= "bidName"> {<text style={{fontWeight: "bold"}}>Action: </text>}
-                    {
-                        <select >
-                            {/*if Approve is chosen a pop-up should appear to warn about Approving the offer permanently */}
-                            <option value="Approve">Approved</option>
-                            {/*if Negotiate is chosen a pop-up should appear to update the current price */}
-                            <option value="Negotiate">Negotiate</option>
-                            {/*if Decline is chosen a pop-up should appear to warn about declining the offer permanently */}
-                            <option value="Decline">Decline</option>
-
-                        </select>
-                    }
-
+                    bid.status === api.data.bids.approved ? (<label style={{color: "green"}}>Approved</label>) :
+                    bid.status === api.data.bids.ownerNegotiate ? (<label style={{color: "orange"}}>Negotiation (waiting on customer)</label>) :
+                    bid.status === api.data.bids.customerNegotiate && bid.approvedByMe ? (<label style={{color: "orange"}}>Negotiation (waiting on staff)</label>) :
+                    bid.status === api.data.bids.customerNegotiate ? (<label style={{color: "orange"}}>Negotiation (waiting on you)</label>) :
+                    bid.status === api.data.bids.declined ? (<label style={{color: "red"}}>Declined</label>) :
+                    null
+                }
                 </p>
 
-                <button className="button primary" style={{margin: "2rem"}}>Apply Action</button>
+                <p className="bidName" style={{ visibility: isInNegotiation ? 'visible' : 'hidden' }}>
+                    <text style={{fontWeight: "bold"}}>Action: </text>
+                    <select>
+                        {/*if Approve is chosen a pop-up should appear to warn about Approving the offer permanently */}
+                        {bid.status === api.data.bids.customerNegotiate ? (<option value="Approve">Approve</option>) : null}
+                        {/*if Negotiate is chosen a pop-up should appear to update the current price */}
+                        <option value="Negotiate">Negotiate</option>
+                        {/*if Decline is chosen a pop-up should appear to warn about declining the offer permanently */}
+                        <option value="Decline">Decline</option>
+                    </select>
+                </p>
+
+                <button className="button primary" style={{
+                    margin: "2rem",
+                    visibility: isInNegotiation ? 'visible' : 'hidden'
+                }}>Apply Action</button>
 
             </div>
         );
