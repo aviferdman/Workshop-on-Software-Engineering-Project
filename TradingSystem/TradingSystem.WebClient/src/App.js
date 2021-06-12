946cb859860e2d5acc879194994e9deb1a57df54
boaz23 <boaz23mw@gmail.com>
2021-06-12 12:52:44 +0300

    Client - app.js code reformat

--- TradingSystem/TradingSystem.WebClient/src/App.js
+++ TradingSystem/TradingSystem.WebClient/src/App.js
@@ -7,9 +7,9 @@ import './App.css'
 import {__RouterContext} from 'react-router'
 
 export function useTitle(title) {
-  useEffect(() => {
-    document.title = 'Ecommerce - ' + title;
-  }, [title]);
+    useEffect(() => {
+        document.title = 'Ecommerce - ' + title;
+    }, [title]);
 }
 
 // TODO: make it flexible based on environment build
@@ -17,155 +17,156 @@ export function useTitle(title) {
 axios.defaults.baseURL = 'https://localhost:5001/api';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.handleCloseErrorDialog = this.handleCloseErrorDialog.bind(this);
-    this.setUsername = this.setUsername.bind(this);
-    this.setWebSocket = this.setWebSocket.bind(this);
-    this.logout = this.logout.bind(this);
+        this.handleCloseErrorDialog = this.handleCloseErrorDialog.bind(this);
+        this.setUsername = this.setUsername.bind(this);
+        this.setWebSocket = this.setWebSocket.bind(this);
+        this.logout = this.logout.bind(this);
 
-    this.state = {
-      showErrorDialog: false,
-      errorMessage: null,
-      globalContext: {
-        username: '',
-        setUsername: this.setUsername,
-        role: null,
-        webSocket: null,
-        setWebSocket: this.setWebSocket,
-        logout: this.logout,
-      }
-    };
-  }
-
-  logoutCore = async (unmounting) => {
-    let newUsername = '';
-    try {
-      let promise = axios.post('/UserGateway/Logout/', {
-        username: this.state.globalContext.username,
-      });
-      if (!unmounting) {
-        let response = await promise;
-        newUsername = response.data;
-      }
-    }
-    catch (e) {
-      newUsername = '';
+        this.state = {
+            showErrorDialog: false,
+            errorMessage: null,
+            globalContext: {
+                username: '',
+                setUsername: this.setUsername,
+                role: null,
+                webSocket: null,
+                setWebSocket: this.setWebSocket,
+                logout: this.logout,
+            }
+        };
     }
 
-    return newUsername;
-  }
-
-  releaseContext = (unmounting) => {
-    this.logoutCore(unmounting).then(newUsername => {
-      this.setState({
-        globalContext: {
-          ...this.state.globalContext,
-          username: newUsername,
-          role: unmounting ? null : UserRole.guest,
-          webSocket: null
+    logoutCore = async (unmounting) => {
+        let newUsername = '';
+        try {
+            let promise = axios.post('/UserGateway/Logout/', {
+                username: this.state.globalContext.username,
+            });
+            if (!unmounting) {
+                let response = await promise;
+                newUsername = response.data;
+            }
         }
-      })
-    });
-  }
-
-  setUsername = (username, role) => {
-    this.setState({
-      globalContext: {
-        ...this.state.globalContext,
-        username: username,
-        role: role
-      }
-    });
-  }
-
-  logout = () => {
-    if (this.state.globalContext.webSocket) {
-      this.state.globalContext.webSocket.close(1000, 'logout');
-    }
-    this.releaseContext(false);
-    this.context.history.push('/login');
-  }
-
-  setWebSocket = username => {
-    // Create WebSocket connection.
-    const socket = new WebSocket('wss://localhost:5001/login');
-
-    // Connection opened
-    socket.addEventListener('open', function (event) {
-      console.log('web socket connection opened');
-      socket.send(username);
-      console.log('sent username');
-    });
-
-    // Listen for messages
-    socket.addEventListener('message', function (event) {
-        alert(`Live notification: ${event.data}`);
-        console.log('web socket message from server', event.data);
-    });
-
-    this.setState({
-      globalContext: {
-        ...this.state.globalContext,
-        webSocket: socket
-      }
-    });
-  }
-
-  handleCloseErrorDialog = () => {
-    this.setState({
-      showErrorDialog: false
-    })
-  };
-
-  async componentDidMount() {
-    await this.init();
-  }
-
-  componentWillUnmount() {
-    this.releaseContext(true);
-  }
-
-  render() {
-    return (
-      <GlobalContext.Provider value={this.state.globalContext}>
-        <Routes />
-        <SimpleAlertDialog message={this.state.errorMessage} isShown={this.state.showErrorDialog} onClose={this.handleCloseErrorDialog} />
-      </GlobalContext.Provider>
-    );
-  }
-
-  async init() {
-    try {
-      let response = await axios.post('/UserGateway/AddGuest/', { });
-      this.setState({
-        showErrorDialog: false,
-        errorMessage: null,
-      });
-      this.setUsername(response.data, UserRole.guest);
-    }
-    catch (e) {
-      this.setUsername('', null);
-      if (e.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        if (e.response.status === 400) {
-          this.setState({
-            errorMessage: 'Failed to connect as a guest.'
-          });
+        catch (e) {
+            newUsername = '';
         }
-      }
-      if (!this.state.errorMessage) {
-        this.setState({
-          errorMessage: 'Unknown error occurred: ' + e.message
+
+        return newUsername;
+    }
+
+    releaseContext = (unmounting) => {
+        this.logoutCore(unmounting).then(newUsername => {
+            this.setState({
+                globalContext: {
+                    ...this.state.globalContext,
+                    username: newUsername,
+                    role: unmounting ? null : UserRole.guest,
+                    webSocket: null
+                }
+            })
         });
-      }
-      this.setState({
-        showErrorDialog: true
-      });
     }
-  }
+
+    setUsername = (username, role) => {
+        this.setState({
+            globalContext: {
+                ...this.state.globalContext,
+                username: username,
+                role: role
+            }
+        });
+    }
+
+    logout = () => {
+        if (this.state.globalContext.webSocket) {
+            this.state.globalContext.webSocket.close(1000, 'logout');
+        }
+        this.releaseContext(false);
+        this.context.history.push('/login');
+    }
+
+    setWebSocket = username => {
+        // Create WebSocket connection.
+        const socket = new WebSocket('wss://localhost:5001/login');
+
+        // Connection opened
+        socket.addEventListener('open', function (event) {
+            console.log('web socket connection opened');
+            socket.send(username);
+            console.log('sent username');
+        });
+
+        // Listen for messages
+        socket.addEventListener('message', function (event) {
+            alert(`Live notification: ${event.data}`);
+            console.log('web socket message from server', event.data);
+        });
+
+        this.setState({
+            globalContext: {
+                ...this.state.globalContext,
+                webSocket: socket
+            }
+        });
+    }
+
+    handleCloseErrorDialog = () => {
+        this.setState({
+            showErrorDialog: false
+        })
+    };
+
+    async componentDidMount() {
+        await this.init();
+    }
+
+    componentWillUnmount() {
+        this.releaseContext(true);
+    }
+
+    render() {
+        return (
+            <GlobalContext.Provider value={this.state.globalContext}>
+                <Routes/>
+                <SimpleAlertDialog message={this.state.errorMessage} isShown={this.state.showErrorDialog}
+                                   onClose={this.handleCloseErrorDialog}/>
+            </GlobalContext.Provider>
+        );
+    }
+
+    async init() {
+        try {
+            let response = await axios.post('/UserGateway/AddGuest/', {});
+            this.setState({
+                showErrorDialog: false,
+                errorMessage: null,
+            });
+            this.setUsername(response.data, UserRole.guest);
+        }
+        catch (e) {
+            this.setUsername('', null);
+            if (e.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                if (e.response.status === 400) {
+                    this.setState({
+                        errorMessage: 'Failed to connect as a guest.'
+                    });
+                }
+            }
+            if (!this.state.errorMessage) {
+                this.setState({
+                    errorMessage: 'Unknown error occurred: ' + e.message
+                });
+            }
+            this.setState({
+                showErrorDialog: true
+            });
+        }
+    }
 }
 
 App.contextType = __RouterContext;
